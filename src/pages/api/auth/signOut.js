0a0555452122d90dc1044706000f5ff9a5548072
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2023-07-28 01:11:57 -0700

    first commit

--- src/pages/api/auth/signOut.js
+++ src/pages/api/auth/signOut.js
@@ -1,9 +1,7 @@
-import SignOut from "../../components/SignOut";
 
 const SignOutPage = () => {
     return (
         <div className="container">
-            <SignOut />
         </div>
     );
 };
